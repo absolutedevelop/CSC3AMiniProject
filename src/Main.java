33a514e36bf3ba9e1c7ab3ee86347bdfcf678e22
absolutedevelop <mathalefortunate5@gmail.com>
2020-03-03 10:14:07 +0200

    Added main file

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+/**
+* @author Fortunate Mathale
+* The main class for the program 
+*/
+public class Main{
+	/**
+	* Entry point for the program
+	* @param args command line arguments
+	*/
+	public static void main(String[] args){
+		System.out.println("test");
+	}	
+}
